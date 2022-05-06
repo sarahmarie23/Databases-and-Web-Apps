4ce4685e8ea68895420bf9dabe205652d8e4827d
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2022-05-06 13:55:33 -0700

    Added comments

--- Quiz Project/quizSubmissions.sql
+++ Quiz Project/quizSubmissions.sql
@@ -1,26 +1,8 @@
-  DROP TABLE submissions CASCADE CONSTRAINTS;
+-- 'answers' holds onto all the solutions to a quiz, which 
+-- is marked by the quiz_id. It allows there to be multiple
+-- quizzes that users could take, but for now, there's only
+-- one quiz, so only one row in 'answers'
 
-CREATE TABLE submissions
-    (SUB_ID         INTEGER NOT NULL,
-     q_1            CHAR(1) NOT NULL,
-     q_2            CHAR(1) NOT NULL,
-     q_3            CHAR(1) NOT NULL,
-     q_4            CHAR(1) NOT NULL,
-     q_5            CHAR(1) NOT NULL,
-     q_6            CHAR(1) NOT NULL,
-     q_7            VARCHAR2(25) NOT NULL,
-     q_8            VARCHAR2(25) NOT NULL,
-     PRIMARY KEY    (sub_id)
-);
-
-  DROP SEQUENCE id_seq;
-   
-CREATE SEQUENCE id_seq
- START WITH 10000
- INCREMENT BY 5
- NOCACHE
- NOCYCLE;
- 
    DROP TABLE answers CASCADE CONSTRAINTS; 
  
  CREATE TABLE answers
@@ -33,9 +15,44 @@ CREATE SEQUENCE id_seq
      a_6            CHAR(1) NOT NULL,
      a_7            VARCHAR2(25) NOT NULL,
      a_8            VARCHAR2(25) NOT NULL,
-     PRIMARY KEY    (sub_id)
+     PRIMARY KEY    (quiz_id)
 );
 
 INSERT INTO answers
 VALUES
 (1000, '4', '2', '1', '3', '2', '1', 'PORT', 'STARBOARD');
+
+
+-- 'submissions' has a row for every time a user takes a
+-- quiz. It keeps track of their name, which quiz they 
+-- took, and the number correct. Every row gets a unique
+-- submission ID, so a user could take a quiz more than
+-- once and all their responses would be saved.
+
+  DROP TABLE submissions CASCADE CONSTRAINTS;
+
+CREATE TABLE submissions
+    (SUB_ID         INTEGER NOT NULL,
+     quiz_id        INTEGER NOT NULL,
+     name           VARCHAR2(25) NOT NULL,
+     q_1            CHAR(1) NOT NULL,
+     q_2            CHAR(1) NOT NULL,
+     q_3            CHAR(1) NOT NULL,
+     q_4            CHAR(1) NOT NULL,
+     q_5            CHAR(1) NOT NULL,
+     q_6            CHAR(1) NOT NULL,
+     q_7            VARCHAR2(25) NOT NULL,
+     q_8            VARCHAR2(25) NOT NULL,
+     num_correct    INTEGER,
+     PRIMARY KEY    (sub_id),
+     FOREIGN KEY    (quiz_id) REFERENCES answers
+);
+
+  DROP SEQUENCE id_seq;
+   
+CREATE SEQUENCE id_seq
+ START WITH 10000
+ INCREMENT BY 5
+ NOCACHE
+ NOCYCLE;
+ 
